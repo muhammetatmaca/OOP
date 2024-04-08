81abc92326ec491b425f608ce0c6657fe72e1fe9
muhammetatmaca <113444449+muhammetatmaca@users.noreply.github.com>
2024-04-09 02:09:42 +0300

    IClonables-interface-yapısı-genel-kullanım

---
+++ olpak.Iclonables/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace olpak.Iclonables
+{
+    public class Book :ICloneable
+    {
+        public int Id { get; set; }
+        public string Tittle {  get; set; }
+        public double Price { get; set; }
+
+        public object Clone()
+        {
+           //return new Book() { Id=this.Id,Tittle=this.Tittle,Price=this.Price };
+            
+
+           return this.MemberwiseClone();
+        }
+    }
+}
---
+++ olpak.Iclonables/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace olpak.Iclonables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book1 = new Book()
+            {
+                Id = 1,
+                Price = 30.5,
+                Tittle="SUÇ VE KADİR"
+            };
+
+            var book2 = (Book)book1.Clone();
+            book2.Tittle = "SEFİLS";
+            Console.WriteLine(book1.Tittle);
+            Console.WriteLine(book2.Tittle);
+            Console.ReadKey();
+        }
+    }
+
+}
