7ff00345ead2dce2df4d5f3b8760e1de31d827ec
muhammetatmaca <113444449+muhammetatmaca@users.noreply.github.com>
2024-04-08 22:10:01 +0300

    formatable-interface-yapısı-örnek

---
+++ olpak.Iformatable/Employee.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace olpak.Iformatable
+{
+    public class Employee :IFormattable
+    {
+        public int Id { get; set; }
+        public string Tittle {  get; set; }
+        public string FirstName { get; set; }
+        public string LastName {  get; set; }
+
+        public string FullName => $"{Tittle} {FirstName} {LastName}";
+
+        public override string ToString()
+        {
+            return this.ToString("F", CultureInfo.CurrentCulture);
+        }
+
+        public  string ToString(string format)
+        {
+            return this.ToString(format, CultureInfo.CurrentCulture);
+
+        }
+
+        public string ToString(string? format, IFormatProvider? formatProvider)
+        {
+            if (string.IsNullOrEmpty(format))
+            {
+                format = "F";
+            }
+
+            if(formatProvider == null)
+            {
+                formatProvider = CultureInfo.CurrentCulture;
+            }
+
+            switch (format.ToUpper())
+            {
+                case "F":return $"{Id} {Tittle} {FirstName} {LastName}";
+                case "FL": return$"{FirstName} {LastName}";
+
+                default:throw  new FormatException("HATALSINN");
+                   
+            }
+        }
+    }
+}
---
+++ olpak.Iformatable/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Xml.Schema;
+
+namespace olpak.Iformatable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now.ToString("D"));
+            var list = new List<Employee>()
+            {
+                new Employee()
+            {
+                Id = 1,
+                FirstName = "muhammet",
+                LastName = "atmcaca",
+                Tittle = "neymiş"
+            },
+                new Employee()
+            {
+                Id = 23,
+                FirstName = "hakan",
+                LastName = "Test",
+                Tittle = "pirof"
+            },
+                new Employee()
+            {
+                Id = 34,
+                FirstName = "holo",
+                LastName = "kolo",
+                Tittle = "doçent"
+            },
+                new Employee()
+            {
+                Id = 45,
+                FirstName = "zolo",
+                LastName = "moloo",
+                Tittle = "yazarmıyım"
+            }
+
+        };
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.ToString("FL"));
+            }
+            
+            
+            Console.ReadKey();
+        }
+    }
+
+
+
+}
