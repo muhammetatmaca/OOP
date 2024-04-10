3f3e32e42ca4b3a347bab1e89811213fe4bc52eb
muhammetatmaca <113444449+muhammetatmaca@users.noreply.github.com>
2024-04-11 01:43:53 +0300

    comparable-interface-yapsının-genel-kullanımı

---
+++ olpak.Icomparable/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace olpak.Icomparable
+{
+    public class Employee:IComparable
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public decimal Salary { get; set; }
+
+        public int CompareTo(object? obj)
+        {
+            var other = (Employee)obj;
+            if (this.Salary < other.Salary)
+            {
+                return -1;
+            }
+
+            else if (this.Salary.Equals(other.Salary))
+            {
+                return 0;
+            }
+
+            else
+            {
+                return 1;
+            }
+        }
+
+        public override string ToString()
+        {
+           return $"{Id,-5} {FullName,-25} {Salary,-7}";
+        }
+    }
+}
---
+++ olpak.Icomparable/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace olpak.Icomparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employees= new List<Employee>() 
+            {
+                new Employee()
+                {
+                    Id = 1,
+                    FullName="muhammet",
+                    Salary=1222
+                },
+
+                 new Employee()
+                {
+                    Id = 2,
+                    FullName="yununs",
+                    Salary=65756
+                },
+                  new Employee()
+                {
+                    Id = 3,
+                    FullName="kunus",
+                    Salary=345
+                },
+                   new Employee()
+                {
+                    Id = 4,
+                    FullName="cunus",
+                    Salary=234
+                },
+            };
+
+            employees.Sort();
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee);
+            }
+
+        }
+    }
+
+}
