94ddfc5c8e0feee511ab925865012e28bd036b04
muhammetatmaca <113444449+muhammetatmaca@users.noreply.github.com>
2024-04-11 18:04:26 +0300

    IEnumerators-genel-kullanım-yapısı

---
+++ olpak.IEnumerators/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace olpak.IEnumerators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var koleksiyon = new List<Product>() 
+            { 
+                new Product() {Price=4500,ProductId=1,ProductName=" ocak "},
+                new Product() {Price=1500,ProductId=2,ProductName=" buzdolabı "},
+                new Product() {Price=2500,ProductId=3,ProductName=" fırın "},
+                new Product() {Price=4500,ProductId=4,ProductName=" camasir "},
+
+
+
+            };
+
+            foreach (var item in koleksiyon)
+            {
+                Console.WriteLine($"{item.ProductId}"+$"{item.ProductName}"+
+                    $"{item.Price}");
+            }
+
+
+            Console.WriteLine(new string('-',25));
+
+            IEnumerator<Product> enumerator = koleksiyon.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine($"{ enumerator.Current.ProductId}"+
+                    $"{enumerator.Current.ProductName}"+
+                    $"{enumerator.Current.Price}"
+                    );
+            }
+            Console.ReadKey();
+        }
+    }
+}
